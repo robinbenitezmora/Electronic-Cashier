661322a4542bd0bab3b2cd148d421d460521b4d3
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-06 11:21:26 -0500

    Create 'Cashier.java' class

---
+++ cashier/Cashier.java
@@ -0,0 +1,10 @@
+package cashier;
+
+import java.io.IOException;
+
+public class Cashier extends OptionMenu {
+ public static void main(String[] args) throws IOException {
+  OptionMenu optionMenu = new OptionMenu();
+  optionMenu.getLogin();
+ }
+};
